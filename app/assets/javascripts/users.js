c14a75313db8fe0e02f2e1218389fcde50531fe8
dhekzanne <derrickparrocho17@gmail.com>
2016-12-20 06:36:23 +0000

    fixed javascript file error

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,41 +1,40 @@
 $(document).ready(function() {
-    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-    // Watch for a form submission:
-    $("#form-submit-btn").click(function(event) {
-        event.preventDefault();
-        $('input[type=submit]').prop('disabled', true);
-        var error = false;
-        var ccNum = $('#card_number').val(),
-            cvcNum = $('#card_code').val(),
-            expMonth = $('#card_month').val(),
-            expYear = $('#card_year').val();
-        
-    if (!error){
-        //Get this Stripe Token:
-        Stripe.createToken({
-            number: ccNum,
-            cvc: cvcNum,
-            exp_month: expMonth,
-            exp_year: expYear
-        }, stripeResponseHandler);
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
     }
     return false;
-}); // form submission
-
-function stripeResponseHandler(status, response){
-    //Get a reference to the form:
+  }); // form submission
+  
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
     var f = $("#new_user");
-    
-    // get token from the response:
+
+    // Get the token from the response:
     var token = response.id;
-    
+
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    
-    // Submit to Form:
-    f.get(0).submit();
-}
+
+    // Submit the form:
+    f.get(0).submit(); 
+  }
 });
-    
     
  
